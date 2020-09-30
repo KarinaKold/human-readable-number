fb4630a240229cc975ca3a18c440d60ae03de4a3
Karina Koldaeva <71209378+KarinaKold@users.noreply.github.com>
2020-09-30 20:33:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function toReadable (number) {
-  
+    const one = {
+        1: 'one',   2: 'two',
+        3: 'three', 4: 'four',
+        5: 'five',  6: 'six',
+        7: 'seven', 8: 'eight',
+        9: 'nine',  10: 'ten',
+        11: 'eleven', 12: 'twelve',
+        13: 'thirteen', 14: 'fourteen',
+        15: 'fifteen', 16: 'sixteen',
+        17: 'seventeen', 18: 'eighteen',
+        19: 'nineteen',
+    };
+    const two = {
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+    };
+    if (number === 0) return 'zero';
+    if (number < 20) return one[number];
+    let numberToArray = number.toString().split('');
+    let res = '';
+
+    function complete(numberToArray) {
+        let res = two[numberToArray[0].concat('0')];
+        if (numberToArray[1] !== '0') res += ` ${one[numberToArray[1]]}`;
+        return res;
+    }
+    if (numberToArray.length === 2) 
+        return complete(numberToArray);
+    if (numberToArray.length === 3) {
+        res = `${one[numberToArray[0]]} hundred`;
+        let sliced = numberToArray.slice(1).join('');
+        if (sliced === '00') return res;
+        if (sliced < 20) res += ` ${one[+sliced]}`;
+        else res += ` ${complete(sliced)}`;
+    }
+    return res;  
 }
